d376f196a3d85526f869b30fc8646c04726676ef
Akanksha Gupta <akankshagupta98583@gmail.com>
2023-08-19 04:49:07 -0700

    used basic react in a js file episode 1 done

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
-const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "hello world from React!");
+const heading = React.createElement("div", {id: "parent"}, 
+[React.createElement("div", {id: "child1"}, 
+[React.createElement("h1", {}, "I am H1 tag" ), 
+React.createElement("h2", {}, "I am H2 tag" )]),
+React.createElement("div", {id: "child2"}, 
+[React.createElement("h1", {}, "I am H1 tag" ), 
+React.createElement("h2", {}, "I am H2 tag" )])]);
         const root= ReactDOM.createRoot(document.getElementById("root"));
         root.render(heading);
