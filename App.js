fbc74cb46eadb05a39f866b20fd258e5d9c60f9a
Akanksha Gupta <akankshagupta98583@gmail.com>
2023-08-23 22:50:13 -0700

    episode 2 done

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"}, 
 [React.createElement("h1", {}, "I am H1 tag" ), 
