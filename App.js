2996b4713a34f302b0c2f48c7c23afe219149eb4
Akanksha Gupta <akankshagupta98583@gmail.com>
2023-08-29 09:20:42 -0700

    episode 3: React element and component part

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading = React.createElement("div", {id: "parent"}, 
-[React.createElement("div", {id: "child1"}, 
-[React.createElement("h1", {}, "I am H1 tag" ), 
-React.createElement("h2", {}, "I am H2 tag" )]),
-React.createElement("div", {id: "child2"}, 
-[React.createElement("h1", {}, "I am H1 tag" ), 
-React.createElement("h2", {}, "I am H2 tag" )])]);
-        const root= ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+//React element
+const jsxheading = (<h1>Namaste react using JSx</h1>);
+console.log(jsxheading);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+        
+
+//React functional component
+const HeadingComponent = ()=> {
+        return <h1>Namaste functional component</h1>
+}
+// can write like this also
+const HeadingComponent1 = ()=> <h1>Namaste functional component</h1>
+//How to render func. component
+root.render(<HeadingComponent/>);
