762ba565b44a629c20d74cb14dd96cb6dc75203f
Akanksha Gupta <akankshagupta98583@gmail.com>
2024-04-28 23:18:15 +0530

    episode 6- api call channge

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
 import RestaurantCard from "./RestaurantCard.js";
 import resList from "../utils/mockdata.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
     //local state variable
- const [listOfResState, setListOfResState] = useState(resList)
+ const [listOfResState, setListOfResState] = useState([])
+ console.log("Body Rendered");
+ useEffect(()=>{
+    fetchData();
+ },[]);
+
+ const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json(); //convert the data stream to json
+    console.log(json);
+    setListOfResState(json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+ };
+
     return(
             <div className="body">
     <button className="filter-btn" onClick={()=>{
@@ -15,7 +28,10 @@ const Body = () => {
         Top rated restaurant
     </button>
     <div className="res-container">
-    {listOfResState.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} resdata={restaurant}/>))}
+    {listOfResState.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+    {/* {listOfResState.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} resdata={restaurant}/>))} */}
     </div>
     </div>
     )
